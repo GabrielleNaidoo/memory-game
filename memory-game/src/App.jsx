5fed8da63ba274d2ed3f749106af61370475912a
Gabrielle Naidoo <naidoogabrielle2@gmail.com>
2024-01-31 15:53:57 +0200

    Addition of card data

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -12,8 +12,16 @@ function App() {
         <Route
           path="/game"
           element={
-            <div>
-              <h1>I am the game!</h1>
+            <div className="game_container">
+              <div className="player_one player_container">
+                <h1>Player One</h1>
+              </div>
+              <div className="cards_container">
+                <h1>I am the game!</h1>
+              </div>
+              <div className="player_two player_container">
+                <h1>Player Two</h1>
+              </div>
             </div>
           }
         ></Route>
--- memory-game/src/data.js
+++ memory-game/src/data.js
@@ -183,9 +183,189 @@ const cardData = [
     image: import("./images/card-images/diamonds/king.jpg"),
     flipped: false
   },
-
+  //hearts
+  {
+    id: 27,
+    value: 'a',
+    suit: 'hearts',
+    image: import("./images/card-images/hearts/a.jpg"),
+    flipped: false
+  },
+  {
+    id: 28,
+    value: '2',
+    suit: 'hearts',
+    image: import("./images/card-images/hearts/2.jpg"),
+    flipped: false
+  },
+  {
+    id: 29,
+    value: '3',
+    suit: 'hearts',
+    image: import("./images/card-images/hearts/3.jpg"),
+    flipped: false
+  },
+  {
+    id: 30,
+    value: '4',
+    suit: 'hearts',
+    image: import("./images/card-images/hearts/4.jpg"),
+    flipped: false
+  },
+  {
+    id: 31,
+    value: '5',
+    suit: 'hearts',
+    image: import("./images/card-images/hearts/5.jpg"),
+    flipped: false
+  },
+  {
+    id: 32,
+    value: '6',
+    suit: 'hearts',
+    image: import("./images/card-images/hearts/6.jpg"),
+    flipped: false
+  },
+  {
+    id: 33,
+    value: '7',
+    suit: 'hearts',
+    image: import("./images/card-images/hearts/7.jpg"),
+    flipped: false
+  },
+  {
+    id: 34,
+    value: '8',
+    suit: 'hearts',
+    image: import("./images/card-images/hearts/8.jpg"),
+  },
+  {
+    id: 35,
+    value: '9',
+    suit: 'hearts',
+    image: import("./images/card-images/hearts/9.jpg"),
+    flipped: false
+  },
+  {
+    id: 36,
+    value: '10',
+    suit: 'hearts',
+    image: import("./images/card-images/hearts/10.jpg"),
+    flipped: false
+  },
+  {
+    id: 37,
+    value: 'jack',
+    suit: 'hearts',
+    image: import("./images/card-images/hearts/jack.jpg"),
+    flipped: false
+  },
+  {
+    id: 38,
+    value: 'queen',
+    suit: 'hearts',
+    image: import("./images/card-images/hearts/queen.jpg"),
+    flipped: false
+  },
+  {
+    id: 39,
+    value: 'king',
+    suit: 'hearts',
+    image: import("./images/card-images/hearts/king.jpg"),
+    flipped: false
+  },
+  //spades
+  {
+    id: 38,
+    value: 'a',
+    suit: 'spades',
+    image: import("./images/card-images/spades/a.jpg"),
+    flipped: false
+  },
+  {
+    id: 39,
+    value: '2',
+    suit: 'spades',
+    image: import("./images/card-images/spades/2.jpg"),
+    flipped: false
+  },
+  {
+    id: 40,
+    value: '3',
+    suit: 'spades',
+    image: import("./images/card-images/spades/3.jpg"),
+    flipped: false
+  },
+  {
+    id: 41,
+    value: '4',
+    suit: 'spades',
+    image: import("./images/card-images/spades/4.jpg"),
+    flipped: false
+  },
+  {
+    id: 42,
+    value: '5',
+    suit: 'spades',
+    image: import("./images/card-images/spades/5.jpg"),
+    flipped: false
+  },
+  {
+    id: 43,
+    value: '6',
+    suit: 'spades',
+    image: import("./images/card-images/spades/6.jpg"),
+    flipped: false
+  },
+  {
+    id: 44,
+    value: '7',
+    suit: 'spades',
+    image: import("./images/card-images/spades/7.jpg"),
+    flipped: false
+  },
+  {
+    id: 45,
+    value: '8',
+    suit: 'spades',
+    image: import("./images/card-images/spades/8.jpg"),
+  },
+  {
+    id: 46,
+    value: '9',
+    suit: 'spades',
+    image: import("./images/card-images/spades/9.jpg"),
+    flipped: false
+  },
+  {
+    id: 47,
+    value: '10',
+    suit: 'spades',
+    image: import("./images/card-images/spades/10.jpg"),
+    flipped: false
+  },
+  {
+    id: 48,
+    value: 'jack',
+    suit: 'spades',
+    image: import("./images/card-images/spades/jack.jpg"),
+    flipped: false
+  },
+  {
+    id: 49,
+    value: 'queen',
+    suit: 'spades',
+    image: import("./images/card-images/spades/queen.jpg"),
+    flipped: false
+  },
+  {
+    id: 50,
+    value: 'king',
+    suit: 'spades',
+    image: import("./images/card-images/spades/king.jpg"),
+    flipped: false
+  }
 ]
-
 
 
 export default cardData;
